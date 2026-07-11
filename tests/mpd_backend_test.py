6e7ce4692bde57d1868e538ee6fb2c3a75b0fa95
DougDamon <doug.damon@gmail.com>
2026-07-11 15:30:57 -0500

    Add MPD audio backend proof of concept

--- tests/mpd_backend_test.py
+++ tests/mpd_backend_test.py
@@ -1,21 +1,16 @@
-import sys
-from pathlib import Path
-
-PROJECT_ROOT = Path(__file__).resolve().parents[1]
-sys.path.insert(0, str(PROJECT_ROOT))
-
 from common.audio.mpd_player import MPDAudioPlayback
 
 player = MPDAudioPlayback()
 
 print("Status:", player.status())
-print("Song:", player.current_song())
-
 print("Volume:", player.get_volume())
+
 player.volume_down()
 print("Volume after down:", player.get_volume())
 
 player.play()
+
 print("Playing:", player.is_playing())
+print("Song:", player.current_song())
 print("Elapsed:", player.get_elapsed())
 print("Duration:", player.get_duration())
