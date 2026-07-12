e6a12e5ccfa3a2c0063026a706bfe3d48f30eaca
DougDamon <doug.damon@gmail.com>
2026-07-12 11:02:17 -0500

    Replace pygame mixer playback with MPD backend

--- common/audio/player.py
+++ common/audio/player.py
@@ -1,33 +1,38 @@
 import os
-import music_tag 
-#import pandas as pd
-#import pygame
-#from datetime import datetime
+import music_tag
+
 import pygame
-from pygame import mixer
+
 from common.config.settings import ConfigService
 from common.library.music_database import MusicDB
-
+from common.audio.mpd_player import MPDAudioPlayback
 
 class AudioPlayback():
     def __init__(self):
         pygame.init()
+
+        # Keep this event defined for now so the rest of the app does not break.
+        # MPD will not use pygame's endevent, so end-of-track handling will need
+        # a later replacement.
         self.MUSIC_END = pygame.USEREVENT + 100
+
         self.musicDB = MusicDB()
         self.config = ConfigService()
         self.musicRootDirectory = self.config.MusicRootDirectory
+
+        self.mpd = MPDAudioPlayback()
+
         self.CurrentDuration = 0
         self.AudioPlaying = False
         self.Repeat = 'Off'
         self.Shuffle = 'Off'
-        mixer.pre_init(frequency=44100, size=-16, channels=2, buffer=2048)
-        mixer.init()
-        mixer.music.set_endevent(self.MUSIC_END)
+        self.CurrentTrackFile = None
+    def getAudioPlayingStatus(self):
+        self.AudioPlaying = self.mpd.is_playing()
+        return self.AudioPlaying
+    
     def setAudioPlayingStatus(self, Status):
         self.AudioPlaying = Status
-    
-    def getAudioPlayingStatus(self):
-        return self.AudioPlaying
         
     def setTrackToPlay(self,  Track):
         self.Track = Track
@@ -48,36 +53,58 @@ class AudioPlayback():
 #        dfNextTrack = self.musicDB.getNowPlayingNextTrack()
 #        return dfNextTrack
     
-    def setTrack(self,  TrackId,  StartPosition = 0):
+    def setTrack(self, TrackId, StartPosition=0):
         sTrackId = TrackId
         dfTrack = self.musicDB.getTrackFromDB(sTrackId)
-        self.CurrentTrackFile = self.TrackFile = os.path.join(dfTrack['FileLocation'].iloc[0],  dfTrack['FileName'].iloc[0])
-#        self.CurrentTrackID3 = self.getTrackID3Tags(self.CurrentTrackFile)
-        pygame.mixer.music.load(self.CurrentTrackFile)
-        pygame.mixer.music.play()
-        pygame.mixer.music.set_pos(StartPosition)
-        pygame.mixer.music.pause()
     
+        self.CurrentTrackFile = self.TrackFile = os.path.join(
+            dfTrack['FileLocation'].iloc[0],
+            dfTrack['FileName'].iloc[0]
+        )
+    
+        # MPD paths are normally relative to music_directory.
+        # Your database appears to store full file locations, so strip the configured
+        # music root when possible.
+        mpd_track_path = self.CurrentTrackFile
+    
+        if mpd_track_path.startswith(self.musicRootDirectory):
+            mpd_track_path = os.path.relpath(mpd_track_path, self.musicRootDirectory)
+    
+        self.mpd.connect()
+        self.mpd.client.clear()
+#        print("CurrentTrackFile:", self.CurrentTrackFile)
+#        print("musicRootDirectory:", self.musicRootDirectory)
+#        print("mpd_track_path:", mpd_track_path)
+        self.mpd.client.add(mpd_track_path)
+        self.mpd.client.play()
+    
+#        if StartPosition > 0:
+#            self.mpd.seek_current(StartPosition)
+    
+        # Match old pygame behavior: setTrack loads/prepares the track but leaves it paused.
+        self.mpd.pause()
+        self.setAudioPlayingStatus(False)
+
     def playTrack(self):
-        mixer.music.unpause()
+        self.mpd.resume()
         self.setAudioPlayingStatus(True)
     
     def pauseTrack(self):
-        mixer.music.pause()
+        self.mpd.pause()
         self.setAudioPlayingStatus(False)
         
     def rewindTrack(self):
-        mixer.music.play(start=0.0)
+        self.mpd.seek_current(0)
+        self.setAudioPlayingStatus(True)
+    
+    def getCurrentPosition(self):
+        return self.mpd.get_elapsed()
     
     def stopTrack(self):
         pass
         
     def nextTrack(self):
         pass
-        
-    def getCurrentPosition(self):
-        currentPosition = mixer.music.get_pos()/1000
-        return currentPosition
-     
+           
     def getCurrentDuration(self):
         return self.CurrentDuration
